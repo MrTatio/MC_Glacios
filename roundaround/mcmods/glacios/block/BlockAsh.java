f5ddd8bf123f793301bfae92ddc23a1d2f416aae
Roundaround <ees27@zips.uakron.edu>
2014-01-29 23:59:24 -0500

    Added a bunch of empty block files

---
+++ roundaround/mcmods/glacios/block/BlockAsh.java
@@ -0,0 +1,12 @@
+package roundaround.mcmods.glacios.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockAsh extends Block {
+
+    public BlockAsh() {
+        super(Material.field_151595_p); // Sand
+    }
+
+}
---
+++ roundaround/mcmods/glacios/block/BlockAshStone.java
@@ -0,0 +1,12 @@
+package roundaround.mcmods.glacios.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockAshStone extends Block {
+
+    public BlockAshStone(Material p_i45394_1_) {
+        super(p_i45394_1_);
+    }
+
+}
