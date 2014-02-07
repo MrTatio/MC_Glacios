cf8f0d7c6065f46d4b494ac71f6cf8bfdad914aa
Roundaround <esteinkerchner@gmail.com>
2014-02-07 00:01:13 -0500

    More Soul Tree gen code cleanup, ready for modification.

--- src/main/java/roundaround/mcmods/glacios/world/gen/feature/WorldGenGlaciosSoulTree.java
+++ src/main/java/roundaround/mcmods/glacios/world/gen/feature/WorldGenGlaciosSoulTree.java
@@ -23,29 +23,25 @@ public class WorldGenGlaciosSoulTree extends WorldGenAbstractTree {
     }
 
     @Override
-    public boolean generate(World world, Random rand, int x, int y, int z) {
+    public boolean generate(World world, Random rand, int startX, int startY, int startZ) {
         int treeHeight = rand.nextInt(3) + 6;
         boolean validLocation = true;
 
-        if (y >= 1 && y + treeHeight + 1 <= 256) {
-            byte width;
-            int posY;
+        if (startY >= 1 && startY + treeHeight + 1 <= 256) {
+            for (int checkY = startY; checkY <= startY + 1 + treeHeight; ++checkY) {
+                byte width = 1;
 
-            for (int ycheck = y; ycheck <= y + 1 + treeHeight; ++ycheck) {
-                width = 1;
-
-                if (ycheck == y) {
+                if (checkY == startY) {
                     width = 0;
                 }
-
-                if (ycheck >= y + 1 + treeHeight - 2) {
+                if (checkY >= startY + 1 + treeHeight - 2) {
                     width = 2;
                 }
 
-                for (int j1 = x - width; j1 <= x + width && validLocation; ++j1) {
-                    for (posY = z - width; posY <= z + width && validLocation; ++posY) {
-                        if (ycheck >= 0 && ycheck < 256) {
-                            if (!this.isReplaceable(world, j1, ycheck, posY)) {
+                for (int checkX = startX - width; checkX <= startX + width && validLocation; ++checkX) {
+                    for (int checkZ = startZ - width; checkZ <= startZ + width && validLocation; ++checkZ) {
+                        if (checkY >= 0 && checkY < 256) {
+                            if (!this.isReplaceable(world, checkX, checkY, checkZ)) {
                                 validLocation = false;
                             }
                         } else {
@@ -58,29 +54,21 @@ public class WorldGenGlaciosSoulTree extends WorldGenAbstractTree {
             if (!validLocation) {
                 return false;
             } else {
-                Block baseBlock = world.func_147439_a(x, y - 1, z);
+                Block baseBlock = world.func_147439_a(startX, startY - 1, startZ);
 
-                boolean isSoil = baseBlock.canSustainPlant(world, x, y - 1, z, ForgeDirection.UP, (BlockSoulSapling) GlaciosBlocks.soulSapling);
-                if (isSoil && y < 256 - treeHeight - 1) {
-                    baseBlock.onPlantGrow(world, x, y - 1, z, x, y, z);
-                    width = 3;
-                    byte b1 = 0;
-                    int l1;
-                    int posX;
-                    int j2;
-                    int i3;
+                boolean isSoil = baseBlock.canSustainPlant(world, startX, startY - 1, startZ, ForgeDirection.UP, (BlockSoulSapling) GlaciosBlocks.soulSapling);
+                if (isSoil && startY < 256 - treeHeight - 1) {
+                    baseBlock.onPlantGrow(world, startX, startY - 1, startZ, startX, startY, startZ);
+                    int offset;
+                    int heightDiff;
 
-                    for (posY = y - width + treeHeight; posY <= y + treeHeight; ++posY) {
-                        i3 = posY - (y + treeHeight);
-                        l1 = b1 + 1 - i3 / 2;
+                    for (int posY = startY - 3 + treeHeight; posY <= startY + treeHeight; ++posY) {
+                        heightDiff = posY - (startY + treeHeight);
+                        offset = 1 - heightDiff / 2;
 
-                        for (posX = x - l1; posX <= x + l1; ++posX) {
-                            j2 = posX - x;
-
-                            for (int posZ = z - l1; posZ <= z + l1; ++posZ) {
-                                int l2 = posZ - z;
-
-                                if (Math.abs(j2) != l1 || Math.abs(l2) != l1 || rand.nextInt(2) != 0 && i3 != 0) {
+                        for (int posX = startX - offset; posX <= startX + offset; ++posX) {
+                            for (int posZ = startZ - offset; posZ <= startZ + offset; ++posZ) {
+                                if (Math.abs(posX - startX) != offset || Math.abs(posZ - startZ) != offset || rand.nextInt(2) != 0 && heightDiff != 0) {
                                     Block block1 = world.func_147439_a(posX, posY, posZ);
 
                                     if (block1.isAir(world, posX, posY, posZ) || block1.isLeaves(world, posX, posY, posZ)) {
@@ -91,54 +79,54 @@ public class WorldGenGlaciosSoulTree extends WorldGenAbstractTree {
                         }
                     }
 
-                    for (posY = 0; posY < treeHeight; ++posY) {
-                        Block block = world.func_147439_a(x, y + posY, z);
+                    for (int posY = 0; posY < treeHeight; ++posY) {
+                        Block block = world.func_147439_a(startX, startY + posY, startZ);
 
-                        if (block.isAir(world, x, y + posY, z) || block.isLeaves(world, x, y + posY, z)) {
-                            this.func_150516_a(world, x, y + posY, z, GlaciosBlocks.soulLog, 0);
+                        if (block.isAir(world, startX, startY + posY, startZ) || block.isLeaves(world, startX, startY + posY, startZ)) {
+                            this.func_150516_a(world, startX, startY + posY, startZ, GlaciosBlocks.soulLog, 0);
 
                             if (this.vinesGrow && posY > 0) {
-                                if (rand.nextInt(3) > 0 && world.func_147437_c(x - 1, y + posY, z)) {
-                                    this.func_150516_a(world, x - 1, y + posY, z, GlaciosBlocks.iceVine, 8);
+                                if (rand.nextInt(3) > 0 && world.func_147437_c(startX - 1, startY + posY, startZ)) {
+                                    this.func_150516_a(world, startX - 1, startY + posY, startZ, GlaciosBlocks.iceVine, 8);
                                 }
 
-                                if (rand.nextInt(3) > 0 && world.func_147437_c(x + 1, y + posY, z)) {
-                                    this.func_150516_a(world, x + 1, y + posY, z, GlaciosBlocks.iceVine, 2);
+                                if (rand.nextInt(3) > 0 && world.func_147437_c(startX + 1, startY + posY, startZ)) {
+                                    this.func_150516_a(world, startX + 1, startY + posY, startZ, GlaciosBlocks.iceVine, 2);
                                 }
 
-                                if (rand.nextInt(3) > 0 && world.func_147437_c(x, y + posY, z - 1)) {
-                                    this.func_150516_a(world, x, y + posY, z - 1, GlaciosBlocks.iceVine, 1);
+                                if (rand.nextInt(3) > 0 && world.func_147437_c(startX, startY + posY, startZ - 1)) {
+                                    this.func_150516_a(world, startX, startY + posY, startZ - 1, GlaciosBlocks.iceVine, 1);
                                 }
 
-                                if (rand.nextInt(3) > 0 && world.func_147437_c(x, y + posY, z + 1)) {
-                                    this.func_150516_a(world, x, y + posY, z + 1, GlaciosBlocks.iceVine, 4);
+                                if (rand.nextInt(3) > 0 && world.func_147437_c(startX, startY + posY, startZ + 1)) {
+                                    this.func_150516_a(world, startX, startY + posY, startZ + 1, GlaciosBlocks.iceVine, 4);
                                 }
                             }
                         }
                     }
 
                     if (this.vinesGrow) {
-                        for (posY = y - 3 + treeHeight; posY <= y + treeHeight; ++posY) {
-                            i3 = posY - (y + treeHeight);
-                            l1 = 2 - i3 / 2;
+                        for (int posY = startY - 3 + treeHeight; posY <= startY + treeHeight; ++posY) {
+                            heightDiff = posY - (startY + treeHeight);
+                            offset = 2 - heightDiff / 2;
 
-                            for (posX = x - l1; posX <= x + l1; ++posX) {
-                                for (j2 = z - l1; j2 <= z + l1; ++j2) {
-                                    if (world.func_147439_a(posX, posY, j2).isLeaves(world, posX, posY, j2)) {
-                                        if (rand.nextInt(4) == 0 && world.func_147439_a(posX - 1, posY, j2).isAir(world, posX - 1, posY, j2)) {
-                                            this.growVines(world, posX - 1, posY, j2, 8);
+                            for (int posX = startX - offset; posX <= startX + offset; ++posX) {
+                                for (int posZ = startZ - offset; posZ <= startZ + offset; ++posZ) {
+                                    if (world.func_147439_a(posX, posY, posZ).isLeaves(world, posX, posY, posZ)) {
+                                        if (rand.nextInt(4) == 0 && world.func_147439_a(posX - 1, posY, posZ).isAir(world, posX - 1, posY, posZ)) {
+                                            this.growVines(world, posX - 1, posY, posZ, 8);
                                         }
 
-                                        if (rand.nextInt(4) == 0 && world.func_147439_a(posX + 1, posY, j2).isAir(world, posX + 1, posY, j2)) {
-                                            this.growVines(world, posX + 1, posY, j2, 2);
+                                        if (rand.nextInt(4) == 0 && world.func_147439_a(posX + 1, posY, posZ).isAir(world, posX + 1, posY, posZ)) {
+                                            this.growVines(world, posX + 1, posY, posZ, 2);
                                         }
 
-                                        if (rand.nextInt(4) == 0 && world.func_147439_a(posX, posY, j2 - 1).isAir(world, posX, posY, j2 - 1)) {
-                                            this.growVines(world, posX, posY, j2 - 1, 1);
+                                        if (rand.nextInt(4) == 0 && world.func_147439_a(posX, posY, posZ - 1).isAir(world, posX, posY, posZ - 1)) {
+                                            this.growVines(world, posX, posY, posZ - 1, 1);
                                         }
 
-                                        if (rand.nextInt(4) == 0 && world.func_147439_a(posX, posY, j2 + 1).isAir(world, posX, posY, j2 + 1)) {
-                                            this.growVines(world, posX, posY, j2 + 1, 4);
+                                        if (rand.nextInt(4) == 0 && world.func_147439_a(posX, posY, posZ + 1).isAir(world, posX, posY, posZ + 1)) {
+                                            this.growVines(world, posX, posY, posZ + 1, 4);
                                         }
                                     }
                                 }
@@ -156,19 +144,9 @@ public class WorldGenGlaciosSoulTree extends WorldGenAbstractTree {
         }
     }
 
-    private void growVines(World world, int x, int starty, int z, int meta) {
-        this.func_150516_a(world, x, starty, z, GlaciosBlocks.iceVine, meta);
-        int i1 = 4;
-
-        while (true) {
-            --starty;
-
-            if (world.func_147439_a(x, starty, z).isAir(world, x, starty, z) || i1 <= 0) {
-                return;
-            }
-
-            this.func_150516_a(world, x, starty, z, GlaciosBlocks.iceVine, meta);
-            --i1;
+    private void growVines(World world, int x, int y, int z, int meta) {
+        for (int offsetY = 0; !world.func_147439_a(x, y - offsetY, z).isAir(world, x, y - offsetY, z) && offsetY < 4; ++offsetY) {
+            this.func_150516_a(world, x, y - offsetY, z, GlaciosBlocks.iceVine, meta);
         }
     }
 
