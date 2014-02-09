917036b1cc667f283b68ced05407f04eff0011b7
Roundaround <esteinkerchner@gmail.com>
2014-02-09 16:00:20 -0500

    Fixed portal and vine textures.

--- src/main/java/roundaround/mcmods/glacios/block/BlockIceVine.java
+++ src/main/java/roundaround/mcmods/glacios/block/BlockIceVine.java
@@ -1,11 +1,38 @@
 package roundaround.mcmods.glacios.block;
 
 import net.minecraft.block.BlockVine;
+import net.minecraft.world.IBlockAccess;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockIceVine extends BlockVine {
 
     public BlockIceVine() {
         super();
+    }
+
+    // @Override
+    // @SideOnly(Side.CLIENT)
+    // public void registerBlockIcons(IIconRegister iconRegister) {
+    // this.blockIcon = iconRegister.registerIcon(Glacios.MODID + ":" + this.getUnlocalizedName().substring(5));
+    // }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public int getBlockColor() {
+        return 16777215;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public int getRenderColor(int p_149741_1_) {
+        return 16777215;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public int colorMultiplier(IBlockAccess p_149720_1_, int p_149720_2_, int p_149720_3_, int p_149720_4_) {
+        return 16777215;
     }
 
 }
