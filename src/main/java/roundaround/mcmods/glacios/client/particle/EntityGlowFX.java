af531a375b4817f68b0930ea30699757ad400e33
Roundaround <esteinkerchner@gmail.com>
2014-02-15 16:34:56 -0500

    Committing old Glacios files.  BREAKS BUILD

---
+++ src/main/java/roundaround/mcmods/glacios/client/particle/EntityGlowFX.java
@@ -0,0 +1,96 @@
+package glacios.client.particle;
+
+import glacios.core.Glacios;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import net.minecraft.client.particle.EntityFX;
+import net.minecraft.world.EnumSkyBlock;
+import net.minecraft.world.World;
+
+public class EntityGlowFX extends EntityFX {
+
+    class LitBLock {
+        public int x, y, z, light;
+
+        LitBLock(int x0, int y0, int z0) {
+            x = x0;
+            y = y0;
+            z = z0;
+        }
+
+        LitBLock(int x0, int y0, int z0, int light0) {
+            x = x0;
+            y = y0;
+            z = z0;
+            light = light0;
+        }
+    }
+
+    public static Set<LitBLock> lights = new HashSet<LitBLock>();
+    public static Set<LitBLock> resets = new HashSet<LitBLock>();
+
+    public EntityGlowFX(World world, double posX, double posY, double posZ) {
+        super(world, posX, posY, posZ);
+        motionX = 0;
+        motionZ = 0;
+        motionY = -0.01D;
+        particleMaxAge = (int) (8.0D / (Math.random() * 0.8D + 0.2D));
+        setParticleTextureIndex((int) (Math.random() * 8.0D));
+    }
+
+    /*
+     * Called to update the entity's position/logic.
+     */
+    @Override
+    public void onUpdate() {
+        prevPosX = posX;
+        prevPosY = posY;
+        prevPosZ = posZ;
+
+        if (particleAge++ >= particleMaxAge) {
+            setDead();
+            return;
+        }
+
+        moveEntity(motionX, motionY, motionZ);
+        lights.add(new LitBLock((int) posX, (int) posY, (int) posZ, 4 - (int) (particleAge * 4.0D / particleMaxAge)));
+    }
+
+    /*
+     * Will get destroyed next tick.
+     */
+    @Override
+    public void setDead() {
+        super.setDead();
+        resets.add(new LitBLock((int) posX, (int) posY, (int) posZ));
+    }
+
+    public static void clearLights() {
+        if (resets.size() == 0) {
+            return;
+        }
+        for (LitBLock block : resets) {
+            Glacios.mc.theWorld.markBlocksDirtyVertical(block.x, block.z, block.y - 2, block.y + 2);
+        }
+    }
+
+    public static void updateLights() {
+        if (lights.size() == 0) {
+            return;
+        }
+        for (LitBLock block : lights) {
+            int light = (int) (15.0F * Glacios.mc.theWorld.getLightBrightness(block.x, block.y, block.z));
+            light += light <= 15 - block.light ? block.light : 15 - light;
+            Glacios.mc.theWorld.setLightValue(EnumSkyBlock.Sky, block.x, block.y, block.z, light);
+            Glacios.mc.theWorld.updateLightByType(EnumSkyBlock.Sky, block.x - 1, block.y, block.z);
+            Glacios.mc.theWorld.updateLightByType(EnumSkyBlock.Sky, block.x + 1, block.y, block.z);
+            Glacios.mc.theWorld.updateLightByType(EnumSkyBlock.Sky, block.x, block.y - 1, block.z);
+            Glacios.mc.theWorld.updateLightByType(EnumSkyBlock.Sky, block.x, block.y + 1, block.z);
+            Glacios.mc.theWorld.updateLightByType(EnumSkyBlock.Sky, block.x, block.y, block.z - 1);
+            Glacios.mc.theWorld.updateLightByType(EnumSkyBlock.Sky, block.x, block.y, block.z + 1);
+        }
+    }
+
+}
