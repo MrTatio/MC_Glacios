8b19d6850d198a8eca57e1b04e1c989043a288fc
Roundaround <esteinkerchner@gmail.com>
2014-03-03 00:08:03 -0500

    Committing old stuff.

--- src/main/java/roundaround/mcmods/glacios/Glacios.java
+++ src/main/java/roundaround/mcmods/glacios/Glacios.java
@@ -6,25 +6,34 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent.ClientTickEvent;
 
 @Mod(modid = Glacios.MODID, name = Glacios.NAME, version = Glacios.VERS)
 public class Glacios {
     public static final String NAME = "Glacios";
-	public static final String MODID = "glacios";
-	public static final String VERS = "0.15a";
-	
-	@EventHandler
-	public void preload(FMLPreInitializationEvent evnt) {
+    public static final String MODID = "glacios";
+    public static final String VERS = "0.15a";
+
+    @EventHandler
+    public void preload(FMLPreInitializationEvent evnt) {
         GlaciosConfig.init(evnt.getSuggestedConfigurationFile());
-        
+
         GlaciosBlocks.init();
         GlaciosItems.init();
         GlaciosBiomes.init();
-	}
-	
-	@EventHandler
-	public void load(FMLInitializationEvent evnt) {
-	    DimensionManager.registerProviderType(GlaciosConfig.dimID, WorldProviderGlacios.class, false);
-	    DimensionManager.registerDimension(GlaciosConfig.dimID, GlaciosConfig.dimID);
-	}
+
+        // FMLCommonHandler.instance().bus().register(this);
+    }
+
+    @EventHandler
+    public void load(FMLInitializationEvent evnt) {
+        DimensionManager.registerProviderType(GlaciosConfig.dimID, WorldProviderGlacios.class, false);
+        DimensionManager.registerDimension(GlaciosConfig.dimID, GlaciosConfig.dimID);
+    }
+
+    @SubscribeEvent
+    public void onTick(ClientTickEvent tick) {
+        
+    }
 }
